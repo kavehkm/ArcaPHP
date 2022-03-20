ce85bc9edd6b731606e726c1a5db16900919dc17
kavehkm <mehrbaniankaveh@gmail.com>
2022-03-20 03:12:26 -0400

    add public/index.php as index and also create 00_environment(pare development environment)

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table of Contents</title>
+    <style>
+        *{
+            font-family: 'Courier New', Courier, monospace;
+        }
+    </style>
+</head>
+<body>
+    <h1>Table of Contents:</h1>
+    <ol>
+        <li>
+            <a href="/00_environment">Environment</a>
+        </li>
+    </ol>
+</body>
+</html>
